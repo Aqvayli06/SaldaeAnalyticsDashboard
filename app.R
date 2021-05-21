bdcfc98ddde58bf1ee55349dd2efd062cc335c0c
Farid Azouaou <28296521+Aqvayli06@users.noreply.github.com>
2021-05-21 14:50:41 +0200

    test

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ SaldaeAnalyticsDashboard::run_app() # add parameters here (if any)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
+
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
 SaldaeAnalyticsDashboard::run_app() # add parameters here (if any)
